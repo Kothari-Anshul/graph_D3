70c6b9831b26d012874e10c1c95494bbee83737f
Sagar Patke <sagarpatke@gmail.com>
2018-02-23 20:02:06 +0530

    Review Comments

--- js/initial.js
+++ js/initial.js
@@ -1,10 +1,10 @@
 // Remove this function ---- fixed
-const readline = require('readline'); // readline ---- fixed
+const readline = require('readline');
 const fs = require('fs');
 
 const fileNames = ['../csv/India2011.csv', '../csv/IndiaSC2011.csv', '../csv/IndiaST2011.csv'];
 const labels = ['GEN', 'ST', 'SC'];
-const agegroup = {}; // FIXME: User proper variable name ---- fixed
+const agegroup = {};
 const eduCategory = {};
 let fileRead = 0;
 
@@ -42,16 +42,16 @@ for (let i = 0; i < fileNames.length; i += 1) { // FIXME: Replace with forEach -
     input: fs.createReadStream(fileNames[i]),
   });
     // const lr = new LineByLineReader(fileNames[i]);
-  lr.line_count = 0;
+  lr.line_count = 0; // FIXME: Use boolean to identify header
   lr.label = labels[i];
 
-  lr.on('line', function fo(line) {
+  lr.on('line', function fo(line) { // FIXME: Unnecessary named function. Use Arrow function instead.
     this.line_count += 1;
     if (this.line_count === 1) {
       /* using header to avoid hardcoding in the else section ---- fixed */
       const values = line.split(',');
 
-      for (let j = 0; j < values.length; j += 1) {
+      for (let j = 0; j < values.length; j += 1) { // FIXME: Avoid for loop.
         if (values[j] === 'Total/ Rural/ Urban') {
           this.total_index = j;
         } else if (values[j] === 'Age-group') {
@@ -64,7 +64,7 @@ for (let i = 0; i < fileNames.length; i += 1) { // FIXME: Replace with forEach -
       const values = line.split(',');
       if (values[this.total_index] === 'Total' && values[this.age_index] !== 'Age not stated' && values[this.age_index] !== 'All ages') { // FIXME: Remove column number hard codings
         if (values[this.age_index] in agegroup) {
-          agegroup[values[this.age_index]] += parseInt(values[this.literate_index], 10);
+          agegroup[values[this.age_index]] += parseInt(values[this.literate_index], 10); // FIXME: Reduce Code Duplication here!
         } else {
           agegroup[values[this.age_index]] = parseInt(values[this.literate_index], 10);
         }
@@ -74,7 +74,7 @@ for (let i = 0; i < fileNames.length; i += 1) { // FIXME: Replace with forEach -
 
       if (this.label in eduCategory) {
         // console.log(this.label);
-        eduCategory[this.label] += parseInt(values[this.literate_index], 10);
+        eduCategory[this.label] += parseInt(values[this.literate_index], 10); // FIXME: Reduce Code Duplication here.
       } else {
         eduCategory[this.label] = parseInt(values[this.literate_index], 10);
       }

90f8c7ec1dace3aae437b58b77a63f61ca6b51b1
Anshul Kothari <anshulkothari96@gmail.com>
2018-02-23 11:03:12 +0530

    changes made after code review by Sagar

--- js/initial.js
+++ js/initial.js
@@ -1,91 +1,77 @@
+// Remove this function ---- fixed
+const readline = require('readline'); // readline ---- fixed
+const fs = require('fs');
 
-function read_file(){
-	var LineByLineReader = require('line-by-line');
-    
-    
-
-	var file_names = ["India2011.csv", "IndiaSC2011.csv", "IndiaST2011.csv"];
-	var labels = ["GEN","ST","SC"];
-	var dict = {};
-	var edu_category = {};
-	var fileRead = 0;
-	
-	var i  = 0;
-	for(i=0; i<file_names.length; i++){
-		
-		var lr = new LineByLineReader(file_names[i]);
-		lr.line_count = 0;
-		lr.label = labels[i];
-		lr.on('line', function (line) {
-			this.line_count++;
-			if(this.line_count == 1){
-				/* do nothing its header section */
-			}
-			else{
-				var values = line.split(",");
-				if(values[4] == "Total" && values[5] != "Age not stated" && values[5] != "All ages")
-				{	if(values[5] in dict){
-						dict[values[5]] += parseInt(values[12]);
-					}
-					else{
-						dict[values[5]] = parseInt(values[12]);
-					}
-					
-				}
-				 /*for reading data according to education category */
-				 
-				
-				 if(this.label in edu_category){
-				 	//console.log(this.label);
-				 	edu_category[this.label] += parseInt(values[12]);
-					
-				}else{
-					edu_category[this.label] = parseInt(values[12]);
-
-					
-					
-				}
-
-			}
-			
-			
-
-		});
-
-		lr.on('end', function () {
-			fileRead++;
-			
-			if(fileRead == 3){
-				
-				
-				/* Create first file to plot for i.e agegroup versus number of literate persons*/
-				var fs = require('fs');
-				fs.writeFile("plot1.json", JSON.stringify(dict), function(err) {
-	    		if(err) {
-	        		return console.log(err);
-	    		}
-
-	    		
-				}); 
-				/* Create second file to plot for category versus number of literate persons*/
-				var fs = require('fs');
-				fs.writeFile("plot2.json", JSON.stringify(edu_category), function(err) {
-	    		if(err) {
-	        		return console.log(err);
-	    		}
-
-	    		
-				}); 
-			}
-
-		});
-	}
+const fileNames = ['India2011.csv', 'IndiaSC2011.csv', 'IndiaST2011.csv'];
+const labels = ['GEN', 'ST', 'SC'];
+const agegroup = {}; // FIXME: User proper variable name ---- fixed
+const eduCategory = {};
+let fileRead = 0;
 
 
+for (const i in fileNames) { // FIXME: Replace with Array.prototype.forEach --- fixed
+  	const lr = readline.createInterface({
+  	input: fs.createReadStream(fileNames[i]),
+  });
+    // const lr = new LineByLineReader(fileNames[i]);
+  lr.line_count = 0;
+  lr.label = labels[i];
+
+  lr.on('line', function (line) {
+    this.line_count++;
+    if (this.line_count === 1) {
+      /* using header to avoid hardcoding in the else section ---- fixed */
+      const values = line.split(',');
+
+      for (const j in values) {
+        	if (values[j] === 'Total/ Rural/ Urban') {
+        		this.total_index = j;
+        	} else if (values[j] === 'Age-group') {
+        		this.age_index = j;
+        	} else if (values[j] === 'Literate - Persons') {
+        		this.literate_index = j;
+        	}
+      }
+    } else {
+      const values = line.split(',');
+      if (values[this.total_index] === 'Total' && values[this.age_index] !== 'Age not stated' && values[this.age_index] !== 'All ages') { // FIXME: Remove column number hard codings
+        if (values[this.age_index] in agegroup) {
+          agegroup[values[this.age_index]] += parseInt(values[this.literate_index]);
+        } else {
+          agegroup[values[this.age_index]] = parseInt(values[this.literate_index]);
+        }
+      }
+      /* for reading data according to education category */
 
 
-	
+      if (this.label in eduCategory) {
+        // console.log(this.label);
+        eduCategory[this.label] += parseInt(values[this.literate_index]);
+      } else {
+        eduCategory[this.label] = parseInt(values[this.literate_index]);
+      }
+    }
+  });
 
+  lr.on('close', () => {
+    fileRead++;
 
+    if (fileRead === 3) {
+      /* Create first file to plot for i.e agegroup versus number of literate persons */
+
+      fs.writeFile('plot1.json', JSON.stringify(agegroup), (err) => {
+        if (err) {
+          return console.log(err);
+        }
+      });
+      /* Create second file to plot for category versus number of literate persons */
+
+      fs.writeFile('plot2.json', JSON.stringify(eduCategory), (err) => {
+        if (err) {
+          return console.log(err);
+        }
+      });
+    }
+  });
 }
-read_file();
+
